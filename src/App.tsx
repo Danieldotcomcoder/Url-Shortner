f98abd7ca579eb8301bbb76735779d17f0ec3991
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2023-01-18 09:27:44 +0200

    Install axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import { Input } from './components/input';
 
 function App() {
   return (
     <div className="App">
-     <input />
+     <Input />
     </div>
   );
 }
--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,7 +1,45 @@
-export const input = () => {
+import axios from 'axios';
+
+export const Input = () => {
+
+
+
+  const handlesubmit = (event: any) => {
+    event.preventDefault()
+    // const encodedParams = new URLSearchParams();
+    // encodedParams.append('url', 'https://google.com/');
+  
+    // const options = {
+    //   method: 'POST',
+    //   url: 'https://url-shortener-service.p.rapidapi.com/shorten',
+    //   headers: {
+    //     'content-type': 'application/x-www-form-urlencoded',
+    //     'X-RapidAPI-Key': '03518c50f0mshb2f93a5c3fbb56fp1a5e6ajsn175d3c928506',
+    //     'X-RapidAPI-Host': 'url-shortener-service.p.rapidapi.com',
+    //   },
+    //   data: encodedParams,
+    // };
+  
+    // axios
+    //   .request(options)
+    //   .then(function (response) {
+    //     console.log(response.data);
+    //   })
+    //   .catch(function (error) {
+    //     console.error(error);
+    //   });
+      console.log(event.currentTarget.elements.userInput.value);
+      
+     return event
+  }
+ 
   return (
     <div>
-        <input type={'text'} ></input>
+      <form action="" onSubmit={handlesubmit}>
+        <input type={'text'} placeholder='test' id="userInput"></input>
+        <button type="submit">Submit</button>
+
+      </form>
     </div>
-  )
-}
+  );
+};

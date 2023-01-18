a7abc52a78e6f84f46974fa8340511b9f4940363
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2023-01-18 09:33:49 +0200

    Add handle submit button

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -6,29 +6,29 @@ export const Input = () => {
 
   const handlesubmit = (event: any) => {
     event.preventDefault()
-    // const encodedParams = new URLSearchParams();
-    // encodedParams.append('url', 'https://google.com/');
+    const encodedParams = new URLSearchParams();
+    encodedParams.append('url', event.currentTarget.elements.userInput.value);
   
-    // const options = {
-    //   method: 'POST',
-    //   url: 'https://url-shortener-service.p.rapidapi.com/shorten',
-    //   headers: {
-    //     'content-type': 'application/x-www-form-urlencoded',
-    //     'X-RapidAPI-Key': '03518c50f0mshb2f93a5c3fbb56fp1a5e6ajsn175d3c928506',
-    //     'X-RapidAPI-Host': 'url-shortener-service.p.rapidapi.com',
-    //   },
-    //   data: encodedParams,
-    // };
+    const options = {
+      method: 'POST',
+      url: 'https://url-shortener-service.p.rapidapi.com/shorten',
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'X-RapidAPI-Key': '03518c50f0mshb2f93a5c3fbb56fp1a5e6ajsn175d3c928506',
+        'X-RapidAPI-Host': 'url-shortener-service.p.rapidapi.com',
+      },
+      data: encodedParams,
+    };
   
-    // axios
-    //   .request(options)
-    //   .then(function (response) {
-    //     console.log(response.data);
-    //   })
-    //   .catch(function (error) {
-    //     console.error(error);
-    //   });
-      console.log(event.currentTarget.elements.userInput.value);
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+      
       
      return event
   }
